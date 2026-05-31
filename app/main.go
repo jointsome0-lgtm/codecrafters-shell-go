39b15492af11c6f19619a7b63138b6a8d35f9c5a
agniv <timagniv@gmail.com>
2026-05-31 23:18:53 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,4 +10,13 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	var command string 
+	_, err := fmt.Scanln(&command)
+	if err != nil {
+		fmt.Println("Reding input error:", err)
+		return
+	}
+
+	fmt.Printf("%v: command not found", command)
 }
